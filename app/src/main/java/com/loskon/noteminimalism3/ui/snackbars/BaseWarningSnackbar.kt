602d56351a0714f3ab3b2aaaae410ce136cbcf44
Loskon <andreyrochev23@gmail.com>
2021-12-13 15:12:52 +0300

    Изменен способ получения контекста

---
+++ app/src/main/java/com/loskon/noteminimalism3/ui/snackbars/BaseWarningSnackbar.kt
@@ -0,0 +1,45 @@
+package com.loskon.noteminimalism3.ui.snackbars
+
+import android.graphics.Color
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import com.loskon.noteminimalism3.R
+import com.loskon.noteminimalism3.managers.setColorBackgroundSnackbar
+import com.loskon.noteminimalism3.utils.getShortFont
+
+/**
+ * Основа для Snackbar с текстом
+ */
+
+object BaseWarningSnackbar {
+
+    private var snackbar: Snackbar? = null
+
+    fun make(
+        parent: ViewGroup, anchorView: View,
+        message: String, isSuccess: Boolean
+    ) {
+        snackbar = Snackbar.make(parent, message, Snackbar.LENGTH_LONG)
+        snackbar?.settingSnackbarView(parent, isSuccess)
+        snackbar?.anchorView = anchorView
+        snackbar?.show()
+    }
+
+    private fun Snackbar?.settingSnackbarView(parent: ViewGroup, isSuccess: Boolean) {
+        val snackView: View? = this?.view
+        val tvSnack: TextView? = snackView?.findViewById(R.id.snackbar_text)
+
+        snackView?.setBackgroundResource(R.drawable.snackbar_background)
+        snackView?.setColorBackgroundSnackbar(parent.context, isSuccess)
+        snackView?.setOnClickListener { dismiss() }
+
+        tvSnack?.setTextColor(Color.WHITE)
+        tvSnack?.typeface = parent.context.getShortFont(R.font.roboto_light)
+    }
+
+    fun dismiss() {
+        snackbar?.dismiss()
+    }
+}

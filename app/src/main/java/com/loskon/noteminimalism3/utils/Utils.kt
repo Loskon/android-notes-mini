002aa3467d9b2cd6255bc743af2a506431662112
Loskon <andreyrochev23@gmail.com>
2021-12-13 15:18:49 +0300

    Добавлено получение ресурсов int и защита от двойного открытия bottomSheet

--- app/src/main/java/com/loskon/noteminimalism3/utils/Utils.kt
+++ app/src/main/java/com/loskon/noteminimalism3/utils/Utils.kt
@@ -6,28 +6,37 @@ import android.graphics.drawable.Drawable
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.FontRes
+import androidx.annotation.IntegerRes
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
 
 /**
  * Утилиты
  */
 
-// Получение ресурсов drawable
 fun Context.getShortDrawable(@DrawableRes icon: Int): Drawable? {
     return ResourcesCompat.getDrawable(resources, icon, null)
 }
 
-
-// Получение ресурсов font
 fun Context.getShortFont(@FontRes font: Int): Typeface? {
     return ResourcesCompat.getFont(this, font)
 }
 
-
-// Получение ресурсов color
 fun Context.getShortColor(@ColorRes color: Int): Int {
     return ContextCompat.getColor(this, color)
+}
+
+fun Context.getShortInt(@IntegerRes int: Int): Int {
+    return resources.getInteger(int)
+}
+
+// Защита от двойного открытия диалогового фрагмента
+fun DialogFragment.onlyShow(fragmentManager: FragmentManager, tag: String) {
+    if (fragmentManager.findFragmentByTag(tag) == null) {
+        show(fragmentManager, tag)
+    }
 }
 
 

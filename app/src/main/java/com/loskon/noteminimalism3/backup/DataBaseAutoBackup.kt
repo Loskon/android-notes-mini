86e2ab8bca8d987e1037812d9fd938e51d03570f
Loskon <andreyrochev23@gmail.com>
2022-01-05 22:05:29 +0500

    Удаление неиспользуемого метода

--- app/src/main/java/com/loskon/noteminimalism3/backup/DataBaseAutoBackup.kt
+++ app/src/main/java/com/loskon/noteminimalism3/backup/DataBaseAutoBackup.kt
@@ -6,7 +6,6 @@ import com.loskon.noteminimalism3.files.CheckCreatedFile
 import com.loskon.noteminimalism3.requests.storage.ResultAccessStorage
 import com.loskon.noteminimalism3.sharedpref.PrefHelper
 import com.loskon.noteminimalism3.ui.toast.ToastControl
-import com.loskon.noteminimalism3.utils.DateUtil
 import com.loskon.noteminimalism3.utils.StringUtil
 import java.io.File
 import java.util.*
@@ -16,56 +15,51 @@ import java.util.*
  */
 
 class DataBaseAutoBackup {
-
     companion object {
 
-        fun createBackupFile(context: Context, date: Date, isShowToast: Boolean) {
+        fun startCreatingBackup(context: Context, date: Date, isShowToast: Boolean) {
             val isAccess: Boolean = ResultAccessStorage.hasAccessStorage(context)
 
             if (isAccess) {
-
-                val folder: File = BackupPath.getBackupFolder(context)
-                val hasCreatedFolder: Boolean = CheckCreatedFile.hasCreated(folder)
-
-                if (hasCreatedFolder) {
-                    performAutoBackup(context, date, isShowToast)
-                } else {
-                    showToast(context, ToastControl.MSG_UNABLE_CREATE_FOLDER, isShowToast)
-                }
-
+                creatingBackupFolder(context, date, isShowToast)
             } else {
-                showToast(context, ToastControl.MSG_TOAST_AUTO_BACKUP_NOT_POSSIBLE, isShowToast)
+                showToast(context, ToastControl.MSG_TOAST_AUTO_BACKUP_NO_PERMISSIONS, isShowToast)
             }
         }
 
-        private fun performAutoBackup(context: Context, date: Date, isShowToast: Boolean) {
+        private fun creatingBackupFolder(context: Context, date: Date, isShowToast: Boolean) {
+            val folder: File = BackupPath.getBackupFolder(context)
+            val hasCreatedFolder: Boolean = CheckCreatedFile.hasCreated(folder)
+
+            if (hasCreatedFolder) {
+                creatingBackupFile(context, date, isShowToast)
+            } else {
+                showToast(context, ToastControl.MSG_TOAST_UNABLE_CREATE_FOLDER, isShowToast)
+            }
+        }
+
+        private fun creatingBackupFile(context: Context, date: Date, isShowToast: Boolean) {
             val backupPath: String = BackupPath.getPathBackupFolder(context)
             val backupName: String = StringUtil.replaceForbiddenCharacters(date)
             val outFileName = "$backupPath$backupName.db"
 
             try {
                 DataBaseBackup.performBackup(context, outFileName)
-                showToastNotification(context, isShowToast)
+                if (isShowToast) showSuccessNotification(context)
             } catch (exception: Exception) {
                 showToast(context, ToastControl.MSG_TOAST_AUTO_BACKUP_FAILED, isShowToast)
             }
-        }
-
-        private fun replaceForbiddenCharacters(date: Date): String {
-            var name: String = DateUtil.getStringDate(date)
-            name = name.replace("[./:]".toRegex(), "_")
-            return "$name (A)"
         }
 
         private fun showToast(context: Context, typeMessage: String, isShowToast: Boolean) {
             if (isShowToast) ToastControl.show(context, typeMessage)
         }
 
-        private fun showToastNotification(context: Context, isShowToast: Boolean) {
+        private fun showSuccessNotification(context: Context) {
             val hasNotification: Boolean = PrefHelper.hasNotificationAutoBackup(context)
 
             if (hasNotification) {
-                showToast(context, ToastControl.MSG_TOAST_AUTO_BACKUP_COMPLETED, isShowToast)
+                showToast(context, ToastControl.MSG_TOAST_AUTO_BACKUP_COMPLETED, true)
             }
         }
     }
